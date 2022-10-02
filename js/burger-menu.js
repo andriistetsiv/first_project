008d80df1c225176285ad8c871b9015a334cb7bf
Andrii Stetsiv <steciv14@gmail.com>
2022-10-02 12:33:16 +0200

    new

---
+++ js/burger-menu.js
@@ -0,0 +1,18 @@
+const burger =  document?.querySelector('.burger');
+const navbar =  document?.querySelector('.navbar');
+const body = document.body
+const navItems = navbar?.querySelectorAll('a')
+
+burger?.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll')
+    burger?.classList.toggle('burger--active');
+    navbar.classList.toggle('navbar--visible');
+});
+
+navItems. forEach(el => {
+    el.addEventListener('click', () => {
+        body.classList.remove('stop-scroll')
+        burger?.classList.remove('burger--active');
+        navbar.classList.remove('navbar--visible');
+    })
+})
